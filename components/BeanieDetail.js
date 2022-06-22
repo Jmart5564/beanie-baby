99030c9cc6a04e9ec2c3cabdface4ffb63b3f740
Jmart5564 <Jmart5564@gmail.com>
2022-06-22 15:09:23 -0700

    adding a link back to main list

--- components/BeanieDetail.js
+++ components/BeanieDetail.js
@@ -9,6 +9,11 @@ export default function createBeanieDetail(root) {
     const birthday = root.querySelector('.birthday');
     const releaseYear = root.querySelector('.release-year');
     const styleNumber = root.querySelector('.style-number');
+    const swingTagGeneration = root.querySelector('.swing-tag');
+    const tushTagGeneration = root.querySelector('.tush-tag');
+    const animal = root.querySelector('.animal');
+    const subTheme = root.querySelector('.sub-theme');
+    const releaseDate = root.querySelector('.release-date');
 
     return ({ beanie }) => {
         img.src = beanie.image;
@@ -19,6 +24,10 @@ export default function createBeanieDetail(root) {
         birthday.textContent = `Birthday: ${beanie.birthday}`;
         releaseYear.textContent = `Release Year: ${beanie.releaseYear}`;
         styleNumber.textContent = `Style Number: ${beanie.styleNumber}`;
-
+        swingTagGeneration.textContent = `Swing Tag Generation: ${beanie.swingTagGeneration}`;
+        tushTagGeneration.textContent = `Tush Tag Generation: ${beanie.tushTagGeneration}`;
+        animal.textContent = `Animal: ${beanie.animal}`;
+        subTheme.textContent = `Subtheme: ${beanie.subtheme}`;
+        releaseDate.textContent = `Release Date: ${beanie.releaseDate}`;
     };
 }
