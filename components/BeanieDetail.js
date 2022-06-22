065efca2aa89edbaa0df26e6a86a29f6d87b40c0
Jmart5564 <Jmart5564@gmail.com>
2022-06-22 14:49:29 -0700

    adding links and details

--- components/BeanieDetail.js
+++ components/BeanieDetail.js
@@ -2,14 +2,23 @@
 
 export default function createBeanieDetail(root) {
     const img = root.querySelector('img');
-    const theme = root.querySelector('.color');
+    const link = root.querySelector('.link');
+    const theme = root.querySelector('.theme');
     const astroSign = root.querySelector('.astro-sign');
+    const color = root.querySelector('.color');
+    const birthday = root.querySelector('.birthday');
+    const releaseYear = root.querySelector('.release-year');
+    const styleNumber = root.querySelector('.style-number');
 
     return ({ beanie }) => {
         img.src = beanie.image;
-        //intro.textContent = 
-        theme.textContent = beanie.theme;
-        astroSign.textContent = `Astrological sign is ${beanie.astroSign}`;
+        link.href = beanie.link;
+        theme.textContent = `Theme: ${beanie.theme}`;
+        astroSign.textContent = `Astrological Sign: ${beanie.astroSign}`;
+        color.textContent = `Color: ${beanie.color}`;
+        birthday.textContent = `Birthday: ${beanie.birthday}`;
+        releaseYear.textContent = `Release Year: ${beanie.releaseYear}`;
+        styleNumber.textContent = `Style Number: ${beanie.styleNumber}`;
 
     };
 }
