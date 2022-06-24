13351fd261b061740e875ac8deb2fcb2aa811d3c
Jmart5564 <Jmart5564@gmail.com>
2022-06-23 17:17:37 -0700

    added Paging and changing how many displayed per page is working

--- app.js
+++ app.js
@@ -2,23 +2,57 @@
 import { getBeanies } from '../services/adopt-service.js';
 
 import createBeanieList from './components/BeanieList.js';
+import createPaging from './components/Paging.js';
+
 // import component creators
+let page = 1;
+let pageSize = 5;
+let totalPages = 0;
 let beanies = [];
 
 async function handlePageLoad() {
-    beanies = await getBeanies();
+    const params = new URLSearchParams(window.location.search);
 
+
+    page = Number(params.get('page')) || 1;
+    pageSize = Number(params.get('pageSize')) || 5;
+
+    const start = (page - 1) * pageSize;
+    const end = (page * pageSize) - 1;
+
+    //beanies = await getBeanies();
+    const { data, count } = await getBeanies({ start, end });
+    beanies = data;
+
+    totalPages = Math.ceil(count / pageSize);
     display();
+}
+
+function handlePaging(change, pageSize) {
+    const params = new URLSearchParams(window.location.search);
+
+    if (pageSize) {
+        page = Math.max(1, page + change);
+    }
+    else {
+        page = 1;
+    }
+
+    params.set('page', page);
+    params.set('pageSize', pageSize);
+    window.location.search = params.toString();
 }
 
 // Create each component: 
 // - pass in the root element via querySelector
 // - pass any needed handler functions as properties of an actions object
 const BeanieList = createBeanieList(document.querySelector('#beanie-list')); 
+const Paging = createPaging(document.querySelector('#paging'), { handlePaging });
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
     // Call each component passing in props that are the pieces of state this component needs
+    Paging({ page, pageSize, totalPages });
     BeanieList({ beanies });
 }
 
--- services/adopt-service.js
+++ services/adopt-service.js
@@ -2,33 +2,35 @@ const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function getBeanies() {
-    const response = await client
+export async function getBeanies({ start, end }) {
+    let query = client
         .from('beanie_babies')
         .select(`
-            id,
-            link,
-            image,
-            theme,
-            astroSign,
-            color,
-            title,
-            birthday,
-            releaseDate,
-            releaseYear,
-            animal,
-            subtheme
-            `);
-        
-
-    return response.data;
+        id,
+        link,
+        image,
+        theme,
+        astroSign,
+        color,
+        title,
+        birthday,
+        releaseDate,
+        releaseYear,
+        animal,
+        subtheme`,
+        { count: 'exact' }
+        );
+    
+    query = query.range(start, end);
+    const response = await query;
+    return response;
 }
 
 export async function getBeanie(id) {
     const response = await client 
         .from('beanie_babies')
         .select()
-        .match({ id })
+        .match({ id: id })
         .single();
 
     return response.data;
